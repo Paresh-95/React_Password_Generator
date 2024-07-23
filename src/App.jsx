fe7364e22129ecbcff9645a77860723523cfdbd4
Paresh-95 <Pareshbhamare95@gmail.com>
2024-07-23 16:33:19 +0530

    beautified

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useCallback, useEffect,useRef } from "react";
 import "./App.css";
+import img1 from "./assets/img1.jpg"
+import toast from "react-hot-toast";
 
 function App() {
   const [length, setlength] = useState(8);
@@ -29,6 +31,7 @@ function App() {
       passwordRef.current?.select()
       // passwordRef.current?.setSelectionRange(0,99)
       window.navigator.clipboard.writeText(pass)
+      toast.success("Copied")
     },[pass])
    
 
@@ -39,62 +42,65 @@ function App() {
   }, [length, numAllowed, charAllowed, passwordGenerator]);
 
   return (
-    <>
-      <div className="w-full max-w-md h-32 mx-auto shadow-md rounded-lg px-4 my-5 text-orange-500 bg-gray-700 ">
-        <h1 className="text-white text-center pt-3 my-3">Password Generator</h1>
-        <div className="flex-shadow rounded-lg overflow-hidden mb-4">
-          <input
-            type="text"
-            value={pass}
-            className="outline-none w-5/6 py-1 px-3 rounded-lg"
-            placeholder="password"
-            ref={passwordRef}
-            readOnly
-          />
-          <button onClick={copyPasswordToClipboard} className=" focus:ring-4 shadow-lg transform active:scale-x-75 transition-transform  outline-none bg-blue-700 text-white shrink-0 py-1 ml-2 px-3 rounded-lg">
-            copy
-          </button>
-        </div>
-        <div className="flex text-sm gap-x-2">
-          <div className="flex items-center gap-x-1">
+      <div className="bg-[#F0F0F0] h-screen flex flex-col md:flex-row ">
+        <div className="w-full max-w-md h-auto mx-auto shadow-md rounded-lg px-4 m-auto text-white bg-[#F87474] ">
+          <h1 className="text-white text-center pt-2 ">Password Generator</h1>
+          <div className="flex-shadow rounded-lg overflow-hidden mb-4">
             <input
-              type="range"
-              min={6}
-              max={100}
-              value={length}
-              className="cursor-pointer"
-              onChange={(e) => {
-                setlength(e.target.value);
-              }}
+              type="text"
+              value={pass}
+              className="outline-none w-5/6 py-1 px-3 text-black rounded-lg"
+              placeholder="password"
+              ref={passwordRef}
+              readOnly
             />
-            <label>Length: {length}</label>
-            <div className="flex items-center ml-2 gap-x-1">
+            <button onClick={copyPasswordToClipboard} className=" focus:ring-4 shadow-lg transform active:scale-x-75 transition-transform  outline-none bg-blue-700 text-white shrink-0 py-1 ml-2 px-3 rounded-lg">
+              copy
+            </button>
+          </div>
+          <div className="flex text-sm gap-x-2 p-2 ">
+            <div className="flex flex-col md:flex-row items-center gap-x-1">
               <input
-                type="checkbox"
-                defaultChecked={numAllowed}
-                id="numberInput"
-                //if we do it like this setnumAllowed(true) then it can be used only once to use it again and again we do it like below
-                onChange={() => {
-                  setnumAllowed((prev) => !prev);
+                type="range"
+                min={6}
+                max={100}
+                value={length}
+                className="cursor-pointer"
+                onChange={(e) => {
+                  setlength(e.target.value);
                 }}
               />
-              <label htmlFor="numberInput">Numbers</label>
-              <div className="flex items-center ml-2 gap-x-1">
+              <label>Length: {length}</label>
+              <div className="flex ml-2 gap-x-1">
                 <input
                   type="checkbox"
-                  defaultChecked={charAllowed}
-                  id="characterInput"
+                  defaultChecked={numAllowed}
+                  id="numberInput"
+                  //if we do it like this setnumAllowed(true) then it can be used only once to use it again and again we do it like below
                   onChange={() => {
-                    setCharAllowed((prev) => !prev);
+                    setnumAllowed((prev) => !prev);
                   }}
                 />
-                <label htmlFor="characterInput">Characters</label>
+                <label htmlFor="numberInput">Numbers</label>
+                <div className="flex items-center ml-2 gap-x-1">
+                  <input
+                    type="checkbox"
+                    defaultChecked={charAllowed}
+                    id="characterInput"
+                    onChange={() => {
+                    setCharAllowed((prev) => !prev);
+                    }}
+                  />
+                  <label htmlFor="characterInput">Characters</label>
+                </div>
               </div>
             </div>
           </div>
         </div>
+      <div className="flex justify-center ml-8">
+        <img src={img1} alt="bg-img " className="h-[300px] w-[350px] md:w-auto md:h-auto "/>
       </div>
-    </>
+    </div>
   );
 }
 
